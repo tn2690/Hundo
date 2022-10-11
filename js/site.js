28279f113894c017d12f5c22e5fd1ed6ba70e1c6
Tom Nguyen <tndallas9517@gmail.com>
2022-10-11 14:24:53 -0500

    221011 added code for Hundo

--- js/site.js
+++ js/site.js
@@ -1,14 +1,66 @@
-function displayMessage() {
+// get starting values from the screen
+// controller function
+function getValues() {
+    // get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
 
-    let msg = document.getElementById("message").value;
+    // parse values
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
 
-    // alert(msg);
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        let numbers = generateNumbers(startValue, endValue);
 
-    Swal.fire( 
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: msg
+        // display results on page
+        displayNumbers(numbers);
+
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Only integers are allowed for Hundo'
+            });
         }
-    )
+}
+
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    for (let i = startValue; i <= endValue; i++) {
+        numbers.push(i);
+    }
+    return numbers;
+}
+
+function displayNumbers(numbers) {
+    let className = 'even';
+    let templateRows = '';
+
+    for (let i = 0; i < numbers.length; i++) {
+        let number = numbers[i];
+
+        if (number % 2 == 0) {
+            className = 'even';
+        } else {
+            className = 'odd';
+        }
+
+        // wraps the table
+        if (i % 10 == 0) {
+            templateRows += "<tr>";
+        }
+
+        // template literal
+        // string interpolation
+        // templateRows += `<tr><td class="${className}">${number}</td></tr>`;
+        templateRows += `<td class="${className}"> ${number} </td>`; // for wrap table
+        
+        if ((i + 1) % 10 == 0) {
+            templateRows += "</tr>";
+        }
+    }
+
+    // innerHTML points to the space between tbody tag
+    document.getElementById('results').innerHTML = templateRows;
 }
